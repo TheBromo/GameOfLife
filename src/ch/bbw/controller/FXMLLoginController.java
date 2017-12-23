6fcd2542c4ba93c5dc7647d72b1f99a9fc1b20f8
ManuelStrenge <manuel.strenge@hotmail.com>
2017-12-23 18:16:49 +0100

    added new window opening

--- src/ch/bbw/controller/FXMLLoginController.java
+++ src/ch/bbw/controller/FXMLLoginController.java
@@ -7,10 +7,17 @@ package ch.bbw.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,10 +29,30 @@ public class FXMLLoginController implements Initializable {
     
     @FXML
     private TextField name;
+    @FXML
+    private Button login;
     
     @FXML
     private void handleButtonLogin(ActionEvent event) {
+        if (!name.getText().equals("")){
+            openLobbyWindow();
+            Stage stage = (Stage) login.getScene().getWindow();
+            stage.close();
+        }
+    }
 
+    private void openLobbyWindow(){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/FXMLLobby.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Test");
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     @Override

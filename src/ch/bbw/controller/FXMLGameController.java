e5042f6b7db3c4a93ef33abec3d39a32f0cc8541
ManuelStrenge <manuel.strenge@hotmail.com>
2018-01-12 18:58:32 +0100

    Started work on canvas

--- src/ch/bbw/controller/FXMLGameController.java
+++ src/ch/bbw/controller/FXMLGameController.java
@@ -9,8 +9,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.transform.Scale;
 
 import java.net.InetAddress;
 import java.net.URL;
@@ -18,15 +21,21 @@ import java.util.Observable;
 import java.util.Observer;
 import java.util.ResourceBundle;
 
-public class FXMLGameController implements Initializable,Observer {
+import static javafx.scene.paint.Color.rgb;
+
+public class FXMLGameController implements Initializable, Observer {
     @FXML
     private Label blueName, redName, blueBlocks, redBlocks;
     @FXML
     private Canvas canvas;
+    private GraphicsContext gc;
     private Client network;
     private Server server;
     private InetAddress serverAdress;
     private CellManager cellManager;
+    private double xOffset, yOffset, zoom, lastDragY, lastDragX;
+    private boolean dragInProgress, zoomed;
+    private Scale norm;
 
 
     @FXML
@@ -38,6 +47,56 @@ public class FXMLGameController implements Initializable,Observer {
 
     @FXML
     private void handleCanvasClick(MouseEvent event) {
+        System.out.println("Click...");
+        if (dragInProgress) {
+            dragInProgress = false;
+            return;
+        }
+    }
+
+
+    @FXML
+    private void handleMouseDrag(MouseEvent event) {
+        System.out.println("Draging...");
+        if (dragInProgress) {
+            if (xOffset + event.getX() - lastDragX <= 380 && xOffset + event.getX() - lastDragX >= -380 && yOffset + event.getY() - lastDragY <= 380 && yOffset + event.getY() - lastDragY >= -380) {
+                xOffset += event.getX() - lastDragX;
+                yOffset += event.getY() - lastDragY;
+            }
+        } else {
+            dragInProgress = true;
+        }
+        lastDragX = event.getX();
+        lastDragY = event.getY();
+        drawBackground(0, 0);
+    }
+
+    @FXML
+    private void handleScroll(ScrollEvent event) {
+        System.out.println("Scrolling..");
+        double zoomFactor = 1.05;
+        double deltaY = event.getDeltaY();
+        if (deltaY < 0) {
+            zoomFactor = 2.0 - zoomFactor;
+        }
+        zoom = zoomFactor;
+        drawBackground(event.getX(), event.getY());
+        zoomed = true;
+    }
+
+    private void drawBackground(double xM, double yM) {
+        gc.setFill(rgb(44, 62, 80));
+        gc.fillRect(0, 0, 100000, 10000);
+        gc.setFill(rgb(52, 73, 94));
+        if (zoomed) {
+            gc.scale(zoom, zoom);
+            zoomed = false;
+        }
+        for (double x = 0; x < canvas.getWidth(); x = x + 20) {
+            for (double y = 0; y < canvas.getHeight(); y = y + 20) {
+                gc.fillRect(x + 2 + xOffset, y + 2 + yOffset, 16, 16);
+            }
+        }
 
     }
 
@@ -46,11 +105,11 @@ public class FXMLGameController implements Initializable,Observer {
         this.network = network;
     }
 
-    public void initServer(Server server){
-        this.server= server;
+    public void initServer(Server server) {
+        this.server = server;
     }
 
-    public void initName(String redName,String blueName){
+    public void initName(String redName, String blueName) {
         this.redName.setText(redName);
         this.blueName.setText(blueName);
     }
@@ -61,8 +120,12 @@ public class FXMLGameController implements Initializable,Observer {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        zoomed = false;
+        zoom = 1;
         cellManager = new CellManager();
+        gc = canvas.getGraphicsContext2D();
 
+        drawBackground(0, 0);
     }
 
     @Override

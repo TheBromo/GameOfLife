2059b5ec44de043488e913c00e1065b680225562
ManuelStrenge <manuel.strenge@hotmail.com>
2018-01-17 00:16:34 +0100

    added test of looking at old fields

---
+++ src/ch/bbw/model/data/Field.java
@@ -0,0 +1,17 @@
+package ch.bbw.model.data;
+
+public class Field {
+    private Cell[][] cells = new Cell[10][10];
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+        for (int x = 0; x < cells.length; x++) {
+            for (int y = 0; y < cells.length; y++) {
+                this.cells[x][y] = cells[x][y].copy();
+            }
+        }
+    }
+}
